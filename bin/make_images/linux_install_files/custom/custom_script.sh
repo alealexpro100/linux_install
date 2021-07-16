e3a8781bb1e926971f4e96e04bfe466794112593
alealexpro100 <alealexn@gmail.com>
2021-07-16 18:04:22 +0800

    FIX: Faster creation of linux_install rootfs.

--- bin/make_images/linux_install_files/custom/custom_script.sh
+++ bin/make_images/linux_install_files/custom/custom_script.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 
-#Enable loading of drivers
-rc-update add firstboot default
-rc-update add modloop sysinit
-
-#Apk setup
+#Enable apk repositories
 sed -i "s/#//g" /etc/apk/repositories
 apk update
-
-#Network setup.
-sed -i 's/#auto/auto/;s/#[ ,\t]*/  /g' /etc/network/interfaces
 
 #Extended fonts for console
 apk add terminus-font
@@ -17,26 +10,21 @@ rc-update add consolefont boot
 sed -i "6s/default8x16.psf.gz/ter-v16n.psf.gz/" /etc/conf.d/consolefont
 sed -i "92s/NO/YES/;92s/#//" /etc/rc.conf
 
-#For linux_install
-apk add bash zstd perl dpkg findmnt lsblk dialog
-mkdir /mnt/mnt
-
-#Disk change tools
-apk add cfdisk e2fsprogs
-
-#Addational soft
-apk add htop rsync
+#Dependencies
+apk add bash dropbear zstd perl dpkg findmnt lsblk dialog cfdisk e2fsprogs wireless-tools wpa_supplicant htop rsync
 
 #SSH
 apk add dropbear
+rc-update add firstboot default
+rc-update add modloop sysinit
 rc-update add dropbear boot
+rc-update del networking boot
 
 #Some other.
 echo "root:pass" | chpasswd -c SHA512
 echo alpine_pxe > /etc/hostname
-echo -e "\n### Welcome to linux_install script! ###\nSSH is working on port 22.\nPassword for root is pass.\n" > /etc/motd
-sed -i "8s/tty1/#tty1/" /etc/inittab
-echo -e "\ntty1::respawn:/bin/login -f root" >> /etc/inittab
+echo -e "\n### Welcome to linux_install! ###\nSSH is working on port 22.\nPassword for root is 'pass'.\n" > /etc/motd
+sed -i "s|tty1.*|tty1::respawn:/bin/login -f root|" /etc/inittab
 sed -i "1s/ash/bash/" /etc/passwd
 
 #Bash profile fix for busybox.
