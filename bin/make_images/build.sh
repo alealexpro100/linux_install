696a9b83aa452a31d57e7948f0747808fc292f19
alealexpro100 <alealexn@gmail.com>
2021-12-11 15:35:43 +0800

    Made build handy.

--- bin/make_images/build.sh
+++ bin/make_images/build.sh
@@ -7,24 +7,19 @@ export ALEXPRO100_LIB_LOCATION="${ALEXPRO100_LIB_LOCATION:-./lib/alexpro100_lib.
 # shellcheck disable=SC1091
 source ./lib/common/lib_connect.sh
 
-if [[ -f ./private_parameters ]]; then
-    li_type=private
-else
-    li_type=public
-fi
-
-BUILD_DEBUG="${BUILD_DEBUG:-0}"
-[[ "$BUILD_DEBUG" == "1" ]] && msg_print warning "Building DEBUG vesrion..."
+LI_TYPE="${LI_TYPE:-private}"
+LI_DEBUG="${LI_DEBUG:-0}"
+[[ "$LI_DEBUG" == "1" ]] && msg_print warning "Building DEBUG vesrion..."
 LI_VERSION="$(cat ./version_install)"
-[[ "$BUILD_DEBUG" == "1" ]] && LI_VERSION="${LI_VERSION}-dbg"
+[[ "$LI_DEBUG" == "1" ]] && LI_VERSION="${LI_VERSION}-dbg"
 ARCH="${ARCH:-x86_64}"
-ALPINE_FILES="${ALPINE_FILES:-../../alpine/v3.14/releases/$ARCH/}"
+ALPINE_FILES="${ALPINE_FILES:-../../alpine/v3.15/releases/$ARCH/}"
 BUILDS_DIR="${BUILDS_DIR:-../linux_install_builds}"
-ALPINE_ISO="$ALPINE_FILES/alpine-standard-3.14.0-$ARCH.iso"
+ALPINE_ISO="$ALPINE_FILES/alpine-standard-3.15.0-$ARCH.iso"
 ALPINE_NETBOOT="$ALPINE_FILES/netboot"
 ALPINE_NEBOOT_VERSION="lts"
-LI_ISO="$BUILDS_DIR/linux_install-$ARCH-$LI_VERSION-$li_type.iso"
-LI_NETBOOT="$BUILDS_DIR/linux_install-$ARCH-$LI_VERSION-$li_type.pxe"
+LI_ISO="$BUILDS_DIR/linux_install-$ARCH-$LI_VERSION-$LI_TYPE.iso"
+LI_NETBOOT="$BUILDS_DIR/linux_install-$ARCH-$LI_VERSION-$LI_TYPE.pxe"
 if [[ -d "$ALPINE_FILES" ]]; then
     msg_print note "Using local directory: $ALPINE_FILES"
 else
@@ -51,12 +46,18 @@ function prepare_initfs() {
 build_files="./bin/make_images/linux_install_files"
 create_tmp_dir make_build
 #Prepare rootfs
-mkdir "$make_build/rootfs" "$make_build/custom"
+mkdir "$make_build/rootfs" "$make_build/custom" 
 cp -r "$build_files/custom/." "$make_build/custom"
 mount -t tmpfs tmpfs "$make_build/rootfs"
 cp "./auto_configs/linux_install_$ARCH.sh" "$make_build/config.sh"
 CUSTOM_DIR="$make_build/custom" default_dir="$make_build/rootfs" "./install_sys.sh" "$make_build/config.sh"
-[[ "$BUILD_DEBUG" == "1" ]] && sed -ie '5s/=0/=1/' "$make_build/rootfs/root/installer.sh"
+[[ "$LI_DEBUG" == "1" ]] && sed -ie '5s/=0/=1/' "$make_build/rootfs/root/installer.sh"
+cp -Rf . "$make_build/rootfs/root/linux_install"
+if [[ "$LI_TYPE" == "public" ]]; then
+    rm -rf "$make_build/rootfs/root/linux_install/private_parameters" \
+    "$make_build/rootfs/root/linux_install/custom/custom_script.sh" \
+    "$make_build/rootfs/root/linux_install/custom/rootfs"
+fi
 squashfs_rootfs_pack "$make_build/rootfs" "$make_build/rootfs.img" xz
 umount "$make_build/rootfs"
 
@@ -74,7 +75,6 @@ make_bootable_iso "$make_build/final_iso" "$LI_ISO"
 rm -rf "$make_build/orig_iso" "$make_build/final_iso" "$make_build/initfs"
 
 #Preapare netboot
-[[ -e "$BUILDS_DIR/linux_install-$ARCH-$(cat ./version_install)-$li_type.pxe" ]] && rm -rf "$BUILDS_DIR/linux_install-$ARCH-$(cat ./version_install)-$li_type.pxe"
 mkdir "$make_build/final_netboot" "$make_build/initfs"
 cp -a "$ALPINE_NETBOOT"/{vmlinuz,initramfs,modloop}-"$ALPINE_NEBOOT_VERSION" "$make_build/final_netboot"
 prepare_initfs "$(find "$make_build/final_netboot" -name "*initramfs-$ALPINE_NEBOOT_VERSION" | head -n 1)" "$make_build/final_netboot/initfs.img"
