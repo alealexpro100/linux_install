e4366f6a794da4d914ec55f96c72ddacb0d43898
alealexpro100 <alealexn@gmail.com>
2022-08-02 13:53:40 +0800

    FIX: Alpine fixes

--- lib/distr/alpine/distr_options.sh
+++ lib/distr/alpine/distr_options.sh
@@ -21,6 +21,6 @@ fi
 read_param "" "$M_MIRROR" "$mirror_alpine" mirror_alpine text_empty
 
 
-read_param "" "$M_DISTR_VER" 'v3.15' version_alpine text
+read_param "" "$M_DISTR_VER" 'v3.16' version_alpine text
 add_var "declare -gx" "preinstall" ""
 read_param "" "$M_PACK" "screen htop rsync bash-completion" postinstall text_empty
