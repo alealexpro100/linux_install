5f59bf22ea1b4732e6a7da340065178a4632ba7b
Aleksey Nasibulin <alealexpro100@ya.ru>
2023-06-26 12:25:26 +0800

    FIX: bypass GRUB bug

--- lib/common/rootfs_script.sh
+++ lib/common/rootfs_script.sh
@@ -50,7 +50,13 @@ function grub_config() {
     [[ -f /usr/lib/grub/i386-efi/modinfo.sh ]] && grub-install --target=i386-efi --efi-directory=/boot --removable $grub_param
     grub-install --target=x86_64-efi --efi-directory=/boot $removable_add $grub_param
   else
-    grub-install --target=i386-pc --force $grub_param $bootloader_bios_place
+    grub-install --target=i386-pc --force $grub_param $bootloader_bios_place || grub_fail=1
+    # May be GRUB bug. See https://askubuntu.com/questions/895632
+    if [[ $grub_fail == "1" ]]; then
+      msg_print warning "GRUB bug detected. See Debian bug 866603.\nDisabling metadata_csum_seed and trying again."
+      tune2fs -O ^metadata_csum_seed $bootloader_bios_place
+      grub-install --target=i386-pc --force $grub_param "$(mount | sed -n "s|\(^/dev/.*\) on ${dir} .*|\1|p")"
+    fi
   fi
   grub-mkconfig -o /boot/grub/grub.cfg
 }
